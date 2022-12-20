aa6cc738d164195052c04451a86fac3ff9bd92a0
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2022-12-20 09:06:56 -0500

    Adding changes

--- main.tf
+++ main.tf
@@ -167,14 +167,6 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 8090
-    to_port     = 8090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
   egress {
     from_port        = 0
     to_port          = 0
