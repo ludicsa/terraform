42c2a98642e94a2cc98463281d0a1fa8ad30e0ad
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2022-12-22 13:34:45 -0500

    Adding changes

--- main.tf
+++ main.tf
@@ -264,7 +264,7 @@ resource "aws_elb" "elastic-load-balancer" {
   cross_zone_load_balancing = true
   idle_timeout              = var.idle_timeout
   tags = {
-    Name = "ELB"
+    Name = "Elastic Load Balancer"
   }
 
 }
