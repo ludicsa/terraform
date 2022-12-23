a0a0c7ccd9647d4128577dde6aabde951a8c0553
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2022-12-23 10:07:31 -0500

    Adding changes to apply.yml

--- test-app.sh
+++ test-app.sh
@@ -1,7 +1,7 @@
 PUBLIC_IP=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
 
-ssh-keyscan -H $PUBLIC_IP
+ssh-keyscan -H $PUBLIC_IP 
 scp -pr -i tfkey /home/ludicsa/.aws/ ubuntu@$PUBLIC_IP:/home/ubuntu/.aws/
 scp -i tfkey /home/ludicsa/terraform/.bashrc ubuntu@$PUBLIC_IP:~
 
-ssh -a -i tfkey ubuntu@$PUBLIC_IP
+ssh -a -i tfkey ubuntu@$PUBLIC_IP /bin/bash
