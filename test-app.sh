92f00e40388c9dcfb0da0845347fba6f05447790
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2022-12-23 00:30:53 -0500

    Adding changes_v5

--- test-app.sh
+++ test-app.sh
@@ -1,5 +1,6 @@
 PUBLIC_IP=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --output=text)
 
+ssh-keyscan -H $PUBLIC_IP
 scp -pr -i tfkey /home/ludicsa/.aws/ ubuntu@$PUBLIC_IP:/home/ubuntu/.aws/
 scp -i tfkey /home/ludicsa/terraform/.bashrc ubuntu@$PUBLIC_IP:~
 
