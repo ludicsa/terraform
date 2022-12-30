196f1fcf11fcdbb84b5fc40c3dcfb8b4b08fc4c0
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2022-12-30 11:57:42 -0500

    Changing cfgs

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "bucket-lucas-ludicsa-final-2"
+    bucket = "bucket-lucas-ludicsa-final-3"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
@@ -228,7 +228,7 @@ resource "aws_autoscaling_group" "auto_scaling_group" {
   tag {
     propagate_at_launch = true
     key                 = "Name"
-    value               = "Application Server"
+    value               = "ASG Server"
   }
 }
 
