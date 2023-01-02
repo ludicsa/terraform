00a0a6fe8f3c1697cc166a75ee3173b68f9c08a1
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2023-01-02 09:38:56 -0500

    Changing NGW name

--- main.tf
+++ main.tf
@@ -79,7 +79,7 @@ resource "aws_nat_gateway" "nat_gateway" {
   allocation_id = aws_eip.nat_gateway.id
   subnet_id     = aws_subnet.publicsubnet_1.id
   tags = {
-    Name = "Nat Gateway"
+    Name = "NGW"
   }
 
 }
