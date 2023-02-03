a5014952daf5a7a92d44d2b66144f4fac30d69cb
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2023-02-03 09:41:08 -0500

    Changing AMI version

--- test.sh
+++
@@ -1,7 +0,0 @@
-export PUBLICIP
-PUBLICIP=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --output=text 2>/dev/null)
-
-scp -i tfkey -o StrictHostKeyChecking=no /home/ludicsa/terraform/.bashrc ubuntu@$PUBLICIP:~
-
-
-ssh -i tfkey -o StrictHostKeyChecking=no ubuntu@$PUBLICIP 2>/dev/null
