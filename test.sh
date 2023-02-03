4baf3ce47a84021b9f00ca75a38a6c3524895951
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2023-02-03 09:16:30 -0500

    Adding test scripts

--- test.sh
+++ test.sh
@@ -1,10 +1,7 @@
 export PUBLICIP
 PUBLICIP=$(aws ec2 describe-instances --query "Reservations[*].Instances[*].PublicIpAddress" --output=text 2>/dev/null)
-echo $PUBLICIP >> ip.txt
 
-IP=$(<ip.txt)
-
-scp -i tfkey -o StrictHostKeyChecking=no /home/ludicsa/terraform/.bashrc ubuntu@$IP:~
+scp -i tfkey -o StrictHostKeyChecking=no /home/ludicsa/terraform/.bashrc ubuntu@$PUBLICIP:~
 
 
-ssh -i tfkey -o StrictHostKeyChecking=no ubuntu@$IP 2>/dev/null
+ssh -i tfkey -o StrictHostKeyChecking=no ubuntu@$PUBLICIP 2>/dev/null
