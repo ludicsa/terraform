58f8f34f38029e34ebfc45116416c2aed4514637
Lucas Ludicsa <lucas.ludicsa99@gmail.com>
2023-03-21 09:40:06 -0300

    Adding changes

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "s3-backend-final-ilegra"
+    bucket = "s3-backend-project-ilegra"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
